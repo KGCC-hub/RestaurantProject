88cb487e240e60a48fefc82aaa04e9d31e386ccd
DESKTOP-330B0V2\Cowardlymango9 <kenia.carrillo@mzpsolutions.com>
2026-03-01 20:50:06 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using RestaurantProject.Models;
+using System.Diagnostics;
+
+namespace RestaurantProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostReservation(Reserva reserva)
+        {
+            var connectionString = _configuration.GetConnectionString("ChainTrust");
+            const string query = @"INSERT INTO Reserva (Nombre, Dia, Hora, Comensales) VALUES (@NameCostumer, @Date, @Time, @NumberOfPeople)";
+            try
+            {
+                using var conn = new SqlConnection(connectionString);
+                using var cmd = new SqlCommand(query, conn)
+                {
+                    CommandTimeout = 300
+                };
+                cmd.Parameters.AddWithValue("@NameCostumer", reserva.NameCostumer);
+                cmd.Parameters.AddWithValue("@Date", reserva.Date);
+                cmd.Parameters.AddWithValue("@Time", reserva.Time);
+                cmd.Parameters.AddWithValue("@NumberOfPeople", reserva.NumberOfPeople);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                throw new ApplicationException(
+                $"Lo sentimos, hubo un error al intentar registar la reserva. Intente de nuevo.: {ex.Message}", ex);
+            }
+            return View("Gracias");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Reserva.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantProject.Models
+{
+    public class Reserva
+    {
+        public string ?NameCostumer { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public int NumberOfPeople { get; set; }
+    }
+}
